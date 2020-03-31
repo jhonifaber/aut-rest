f7887d3757c905865b7fa746acafb3fe49b00349
Jonathan Faber <jonathan.faber@autentia.com>
2020-04-01 00:59:56 +0200

    setting flyway config in application.properties

---
+++ src/main/resources/db-migration/V1__student_table.sql
@@ -0,0 +1,6 @@
+create table student(
+	id int auto_increment,
+	name int null,
+	surname int null,
+	constraint student_pk primary key (id)
+);
---
+++ src/main/resources/db-migration/V2__table_professor.sql
@@ -0,0 +1,6 @@
+create table professor(
+    id      int auto_increment,
+    name    int null,
+    surname int null,
+    constraint professor_pk primary key (id)
+);
