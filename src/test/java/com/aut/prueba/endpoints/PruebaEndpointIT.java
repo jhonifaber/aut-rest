c4b6724d146282cac72949280d80f55e7519ba19
Jonathan Faber <jonathan.faber@autentia.com>
2020-05-22 14:53:58 +0200

    init database before launching IT

--- src/test/java/com/aut/prueba/endpoints/PruebaEndpointIT.java
+++ src/test/java/com/aut/prueba/endpoints/PruebaEndpointIT.java
@@ -2,6 +2,9 @@ package com.aut.prueba.endpoints;
 
 import com.aut.prueba.security.AuthenticationRequest;
 import com.aut.prueba.security.AuthenticationResponse;
+import com.aut.prueba.service.DatabaseService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,13 +15,22 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = "test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class PruebaEndpointIT {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    protected DatabaseService databaseService;
+
     @LocalServerPort
     private int port;
+
+    @BeforeAll
+    private void initDatabase() {
+        databaseService.fill();
+    }
 
     public String createUrlWith(String endpoint) {
         return "http://localhost:" + port + endpoint;
@@ -30,7 +42,7 @@ public abstract class PruebaEndpointIT {
 
     public HttpHeaders makeRequestLogin() {
         AuthenticationRequest loginRequest = AuthenticationRequest.builder().username("any").password("pass").build();
-        ResponseEntity<AuthenticationResponse> loginResponse = getRestTemplate().postForEntity(createUrlWith( "/login"), loginRequest, AuthenticationResponse.class);
+        ResponseEntity<AuthenticationResponse> loginResponse = getRestTemplate().postForEntity(createUrlWith("/login"), loginRequest, AuthenticationResponse.class);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + loginResponse.getBody().getToken());
         return headers;
