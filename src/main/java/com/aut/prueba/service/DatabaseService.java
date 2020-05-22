f0610943bb7d1ca58b749659419c243e209eb143
Jonathan Faber <jonathan.faber@autentia.com>
2020-05-22 12:50:11 +0200

    Clear database before populating from endpoint

--- src/main/java/com/aut/prueba/service/DatabaseService.java
+++ src/main/java/com/aut/prueba/service/DatabaseService.java
@@ -2,17 +2,26 @@ package com.aut.prueba.service;
 
 import com.aut.prueba.model.Room;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class DatabaseService {
 
+    private final JdbcTemplate jdbcTemplate;
     private final RoomService roomService;
 
     public void fill() {
+        clearDatabase();
         Room room1 = Room.builder().number(12L).build();
         roomService.createRoom(room1);
+    }
+
+    private void clearDatabase() {
+        Stream<String> tablesScriptV1 = Stream.of("room", "speaker", "talk");
+        tablesScriptV1.forEach(table -> jdbcTemplate.execute("delete from " + table));
     }
 }
